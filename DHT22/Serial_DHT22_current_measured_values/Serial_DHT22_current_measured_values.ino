7bcdd30eacd2a44894964ee99065dbd759b1ec4e
OndrejKnebl <72308047+OndrejKnebl@users.noreply.github.com>
2022-03-25 19:32:16 +0100

    Add files via upload

---
+++ DHT22/Serial_DHT22_current_measured_values/Serial_DHT22_current_measured_values.ino
@@ -0,0 +1,29 @@
+#include <TroykaDHT.h>  //TroykaDHT
+DHT dht(12, DHT22);     // pin 12 and DTH22 type
+
+float humi = 0.0;       // variable for humidity
+float temp = 0.0;       // variable for temperature
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    dht.read();
+
+    if(dht.getState() == DHT_OK){
+      
+      temp = dht.getTemperatureC();   // current temperature
+      humi = dht.getHumidity();       // current humidity
+
+      Serial.print("Temp: ");
+      Serial.print(temp);
+      Serial.println(" °C");
+
+      Serial.print("Humi: ");
+      Serial.print(humi);
+      Serial.println(" %");
+
+      delay(10000);
+   }
+}
