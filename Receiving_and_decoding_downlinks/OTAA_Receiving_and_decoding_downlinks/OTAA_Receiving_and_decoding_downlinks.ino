9ef10b545563c30eef753588fc920f7d875eb535
OndrejKnebl <72308047+OndrejKnebl@users.noreply.github.com>
2023-02-21 16:21:31 +0100

    Add files via upload

---
+++ Receiving_and_decoding_downlinks/OTAA_Receiving_and_decoding_downlinks/OTAA_Receiving_and_decoding_downlinks.ino
@@ -0,0 +1,223 @@
+/*******************************************************************************
+ * The Things Network - OTAA Feather M0
+ * 
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ * Copyright (c) 2018 Terry Moore, MCCI
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ * 
+ * 
+ * Reference: https://github.com/mcci-catena/arduino-lmic
+ *
+ * Modified for Receiving and decoding downlinks, 21. 2. 2023
+ *******************************************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include <CayenneLPP.h>
+CayenneLPP lpp(51);
+
+
+//--------------------------------------- Here change your keys -------------------------------------------------
+
+static const u1_t PROGMEM APPEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };   // AppEUI, LSB
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}                        
+
+static const u1_t PROGMEM DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };   // DevEUI, LSB
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };  // AppKey, MSB
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+//---------------------------------------------------------------------------------------------------------------
+
+const lmic_pinmap lmic_pins = {           // Pin mapping for the Adafruit Feather M0
+    .nss = 8,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 4,
+    .dio = {3, 6, LMIC_UNUSED_PIN},
+    .rxtx_rx_active = 0,
+    .rssi_cal = 8,
+    .spi_freq = 8000000,
+};
+
+static osjob_t sendjob;                   // Job
+
+static uint8_t mydata[] = "Hello, LoRa!"; // Data
+const unsigned TX_INTERVAL = 30;          // Transmission interval in seconds
+
+
+void printHex2(unsigned v) {
+    v &= 0xff;
+    if (v < 16)
+        Serial.print('0');
+    Serial.print(v, HEX);
+}
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            {
+              u4_t netid = 0;
+              devaddr_t devaddr = 0;
+              u1_t nwkKey[16];
+              u1_t artKey[16];
+              LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+              Serial.print("netid: ");
+              Serial.println(netid, DEC);
+              Serial.print("devaddr: ");
+              Serial.println(devaddr, HEX);
+              Serial.print("AppSKey: ");
+              for (size_t i=0; i<sizeof(artKey); ++i) {
+                if (i != 0)
+                  Serial.print("-");
+                printHex2(artKey[i]);
+              }
+              Serial.println("");
+              Serial.print("NwkSKey: ");
+              for (size_t i=0; i<sizeof(nwkKey); ++i) {
+                      if (i != 0)
+                              Serial.print("-");
+                      printHex2(nwkKey[i]);
+              }
+              Serial.println();
+            }
+            LMIC_setLinkCheckMode(0);       // Disable link check validation
+            LMIC.dn2Dr = DR_SF9;            // TTS uses SF9 for its RX2 window.
+            LMIC_setDrTxpow(DR_SF9,14);     // Set data rate and transmit power for uplink
+            LMIC_setAdrMode(0);             // Adaptive data rate disabled
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.print(F("Received "));
+              Serial.print(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+
+              Serial.print(F("My Data: "));
+              for (int i = LMIC.dataBeg; i < LMIC.dataBeg+LMIC.dataLen; i++)
+                printHex2(LMIC.frame[i]);
+
+              Serial.println();
+
+              DynamicJsonDocument jsonBuffer(1024);
+              JsonObject root = jsonBuffer.to<JsonObject>();
+              byte downlinkData[1024] = {0};
+
+              byte j = 0;
+              for(byte i = LMIC.dataBeg; i < LMIC.dataBeg + LMIC.dataLen; i++){
+                  downlinkData[j] = LMIC.frame[i];
+                  j++;
+              }
+
+              lpp.decodeTTN(downlinkData, LMIC.dataLen, root);
+
+              serializeJsonPretty(root, Serial);
+              Serial.println();
+            }
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);  // Schedule next transmission
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            Serial.println(F("EV_RXCOMPLETE"));   // data received in ping slot
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+        case EV_TXSTART:
+            Serial.println(F("EV_TXSTART"));
+            break;
+        case EV_TXCANCELED:
+            Serial.println(F("EV_TXCANCELED"));
+            break;
+        case EV_RXSTART:
+            /* do not print anything -- it wrecks timing */
+            break;
+        case EV_JOIN_TXCOMPLETE:
+            Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+            break;
+        default:
+            Serial.print(F("Unknown event: "));
+            Serial.println((unsigned) ev);
+            break;
+    }
+}
+
+void do_send(osjob_t* j){
+    if(!(LMIC.opmode & OP_TXRXPEND)){                       // Check if there is not a current TX/RX job running
+        Serial.println("Sending - Hello, LoRa!");           // Print to Serial
+
+        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);    // Prepare upstream data transmission at the next possible time.
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("Starting"));
+
+    os_init();
+    LMIC_reset();
+
+    //EU868
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+
+    LMIC_setLinkCheckMode(0);       // Disable link check validation
+    LMIC.dn2Dr = DR_SF9;            // TTS uses SF9 for its RX2 window.
+    LMIC_setDrTxpow(DR_SF9,14);     // Set data rate and transmit power for uplink
+    LMIC_setAdrMode(0);             // Adaptive data rate disabled
+
+    LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100); 
+
+    do_send(&sendjob);     // Start sendjob
+}
+
+void loop() {
+    os_runloop_once();
+}
